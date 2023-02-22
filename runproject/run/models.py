03e5205a056715929cfe3a654afc215edb0101d7
Alec Hannaford <alec322@gmail.com>
2023-02-21 18:42:06 -0800

    floatfield

--- runproject/run/models.py
+++ runproject/run/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Run(models.Model):
     name = models.CharField(max_length=100)
     dur = models.IntegerField(default = None)
-    distance = models.IntegerField(default = None)
+    distance = models.FloatField(default = None)
     category = models.CharField(max_length=100, default = None)
