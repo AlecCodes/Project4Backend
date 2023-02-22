133ba0b5c67ff8ce7ba0688efa34ada15c961cc2
Alec Hannaford <alec322@gmail.com>
2023-02-21 18:33:04 -0800

    RunViewSet

--- runproject/run/serializers.py
+++ runproject/run/serializers.py
@@ -2,6 +2,6 @@ from .models import Run
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class RunSerializer(serializers.HyperlinkedModelSerializer):
     model = Run
     fields = ['name','dur','distance', 'category']
--- runproject/run/views.py
+++ runproject/run/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import Run
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import RunSerializer
 
 # Create your views here.
+class RunViewSet(viewsets.ModelViewSet):
+    #load into frontend -> filter results per route params -> crunch the numbers -> display analytics
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+    permission_classes = [permissions.AllowAny]
+
